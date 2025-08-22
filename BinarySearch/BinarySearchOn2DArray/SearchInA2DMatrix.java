16ea29f92b99c8903bf1c17a5c686d2849777a29
SahilAGowda <sahilagowda.22cs134.nc@cambridge.edu.in>
2025-08-22 09:34:03 +0530

    search in 2d matrix optimal logic done

--- BinarySearch/BinarySearchOn2DArray/SearchInA2DMatrix.java
+++ BinarySearch/BinarySearchOn2DArray/SearchInA2DMatrix.java
@@ -34,6 +34,8 @@ public class SearchInA2DMatrix {
      * 1 <= m, n <= 100
      * -104 <= matrix[i][j], target <= 104
      */
+
+    //  better Approach 
     public static boolean binarySearch(int arr[], int tar) {
         int low = 0;
         int high = arr.length - 1;
@@ -58,8 +60,31 @@ public class SearchInA2DMatrix {
         }
         return false;
     }
+
+
+
+    // Optimal Approach
+     public static boolean searchMatrix1(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int low = 0;
+        int high = n*m-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            int row = mid/m;
+            int col = mid%m;
+            if(matrix[row][col]==target){
+                return true;
+            }else if(matrix[row][col]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int nums[][]={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        System.out.println(searchMatrix(nums, 3));
+        System.out.println(searchMatrix1(nums, 3));
     }
 }
